d7b3dec76054869b41b2a2532dbf1f4023158db9
diaa <diaa11trad@gmail.com>
2024-04-15 22:27:00 +0300

    NavBar is added

--- Rentify/src/components/HomePage/index.jsx
+++ Rentify/src/components/HomePage/index.jsx
@@ -1,5 +1,11 @@
 // import FAQSection from "../FAQSection";
+import NavigationBar from "../NavigationBar";
 
 export default function HomePage() {
-  return <>{/* <FAQSection /> */}</>;
+  return (
+    <>
+      <NavigationBar />
+      {/* <FAQSection /> */}
+    </>
+  );
 }
---
+++ Rentify/src/components/NavigationBar/index.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import {
+  Nav,
+  Navbar,
+  NavDropdown,
+  Button,
+  Image,
+  Form,
+  Container,
+} from "react-bootstrap";
+import LogoImage from "../../assets/images/RentifyLogo.png";
+import "./styles.css";
+
+export default function NavigationBar() {
+  return (
+    <Navbar expand="lg">
+      <Navbar.Brand href="#home">
+        <Image className="brand-logo" src={LogoImage} alt="Logo" fluid />
+      </Navbar.Brand>
+
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Container className="w-100 d-flex justify-content-between container">
+          <Nav className="lg d-flex navbar-lists">
+            <Nav.Link href="#home">الصفحة الرئيسية</Nav.Link>
+
+            <NavDropdown
+              title="الفئات"
+              id="basic-nav-dropdown"
+              renderMenuOnMount={true}
+            >
+              <NavDropdown.Item href="#action/3.1">الكترونيات</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">ملابس </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">
+                أدوات صيانة
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.4"> أخرى </NavDropdown.Item>
+            </NavDropdown>
+
+            <Nav.Link href="#how-it-works">كيف يعمل الموقع</Nav.Link>
+            <Nav.Link href="#about-us"> من نحن </Nav.Link>
+          </Nav>
+
+          <Nav className="nav-btns">
+            <Button variant="primary" className="mr-2 nav-btn">
+              انشر إعلان جديد
+            </Button>
+            <Button variant="secondary" className="mr-2 nav-btn">
+              إنشاء حساب
+            </Button>
+            <Button variant="outline-secondary" className="nav-btn">
+              تسجيل الدخول
+            </Button>
+          </Nav>
+        </Container>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}

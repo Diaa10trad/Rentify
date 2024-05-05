8201dfd860828e27738b725877fe2ee94cb7e468
MohammedSamhouri20 <66756821+MohammedSamhouri20@users.noreply.github.com>
2024-05-05 20:42:18 +0300

    CTASection is added

--- Rentify/src/views/HomePage/index.jsx
+++ Rentify/src/views/HomePage/index.jsx
@@ -1,11 +1,13 @@
 import FAQSection from "@/components/FAQSection";
 import ExploreCategoriesSection from "@/components/ExploreCategoriesSection";
 import Footer from "@/components/Footer";
+import CTASection from "@/components/CTASection";
 export default function HomePage() {
   return (
     <>
       <FAQSection />
       <ExploreCategoriesSection />
+      <CTASection />
       <Footer />
     </>
   );
